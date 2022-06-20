8c091342cdc44521279b408e5e2b0af438970048
Martin <mkmartinoes@gmail.com>
2022-06-20 09:57:02 +0300

    st: hero section

--- src/app.js
+++ src/app.js
@@ -2,13 +2,13 @@ import React from "react";
 
 function App(){
   return(
-    <div className="container-main">
+    <div className="container-main bg-dark hero">
       <div className="row">
         <div className="col-sm-2">
 
         </div>
         <div className="col-sm-10">
-          
+
         </div>
       </div>
     </div>
