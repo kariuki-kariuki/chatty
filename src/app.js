4d21a1da0691675b3350f81ee5ac43be9994e4c5
Martin <mkmartinoes@gmail.com>
2022-06-20 09:48:39 +0300

    st: page divison

--- src/app.js
+++ src/app.js
@@ -2,7 +2,16 @@ import React from "react";
 
 function App(){
   return(
-    <p>Hello World</p>
+    <div className="container-main">
+      <div className="row">
+        <div className="col-sm-2">
+
+        </div>
+        <div className="col-sm-10">
+          
+        </div>
+      </div>
+    </div>
   )
 }
 
