3702c4cd74f1d9d507eab260ee468805f604b943
Martin <mkmartinoes@gmail.com>
2022-06-20 10:09:57 +0300

    st: nav three button

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 import React from "react";
+import Nav from "./components/navigation/Nav";
 
 function App(){
   return(
     <div className="container-main bg-dark hero">
       <div className="row">
         <div className="col-sm-2">
-
+          <Nav />
         </div>
         <div className="col-sm-10">
 
---
+++ src/components/navigation/Nav.js
@@ -0,0 +1,12 @@
+import "./Nav.css"
+function Nav(){
+  return (
+    <nav>
+      <span className="span-1"></span>
+      <span className="span-2"></span>
+      <span className="span-3"></span>
+    </nav>
+  );
+}
+
+export default Nav
