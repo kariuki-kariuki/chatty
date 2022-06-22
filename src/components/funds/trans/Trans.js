26cf6f613f06fbc2222e9268dcae381584688972
Martin <mkmartinoes@gmail.com>
2022-06-22 17:01:39 +0300

    Transactions Component

--- src/components/funds/trans/Trans.js
+++ src/components/funds/trans/Trans.js
@@ -3,21 +3,15 @@ import "./Trans.css"
 function Trans(){
 
   return (
-    <div className="d-flex justify-content-around p-3 mt-2 bg-black myTable">
-      <div className="trans-image">
-        <img src={require("./download s.jpeg")}/>
+    <div className="d-flex justify-content-around p-1 align-items-center mt-2  myTable">
+      <div className="">
+        <img className="trans-image" src={require("./download s.jpeg")} style={{ width: "20%", height: "auto"}}/>
       </div>
-      <div className="sender">
-        Jeff Bezzos
-      </div>
-      <div className="amount">
-        $200
-      </div>
-      <div className="date">
-        12-2-2022
-      </div>
+      <div className="sender">Jeff Bezzos</div>
+      <div className="amount">$200</div>
+      <div className="date">12-2-2022</div>
       <div className="status-sent">
-          <p>Sent</p>
+        <p>Sent</p>
       </div>
     </div>
   );
